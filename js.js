b44d59530b52068694217edf397e5832e4d6e779
DoooNee <88998905+DoooNee@users.noreply.github.com>
2023-03-14 18:09:03 +0700

    first commit

---
+++ js.js
@@ -0,0 +1,10 @@
+function showDashBoard() {
+    $('.content_dashboard').hide();
+    $('.content_inforDaiLy').show();
+    
+}
+
+function showTTDaiLy() {
+    $('.content_dashboard').show();
+    $('.content_inforDaiLy').hide();
+}
