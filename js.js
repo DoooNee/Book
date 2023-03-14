cb2af5a5b85733b5864898b59210fa236e5e2d8b
DoooNee <88998905+DoooNee@users.noreply.github.com>
2023-03-14 18:23:14 +0700

    title

--- js.js
+++ js.js
@@ -1,10 +1,19 @@
 function showDashBoard() {
     $('.content_dashboard').hide();
     $('.content_inforDaiLy').show();
+
+    $('.title_dashboard').hide();
+    $('.title_DaiLy').show();
+
     
 }
 
 function showTTDaiLy() {
     $('.content_dashboard').show();
     $('.content_inforDaiLy').hide();
+
+
+    $('.title_dashboard').show();
+    $('.title_DaiLy').hide();
+
 }
